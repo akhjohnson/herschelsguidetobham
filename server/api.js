7ee234eb539a0a8c831a4b6f6247a055dfb56fc1
akhjohnson <akhalljohnson@gmail.com>
2017-04-24 18:06:26 -0500

    copied over api.js from other project

---
+++ server/api.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var users = require('./controllers/users.ctrl');
+var objectives = require('./controllers/objectives.ctrl');
+var locations = require('./controllers/locations.ctrl');
+var categories = require('./controllers/categories.ctrl');
+var badges = require('./controllers/badges.ctrl');
+// var purchases = require('./controllers/purchases.ctrl');
+
+
+var router = express.Router();
+
+router.use('/users', users);
+router.use('/objectives', objectives);
+router.use('/locations', locations);
+router.use('/categories', categories);
+router.use('/badges', badges);
+// router.use('/purchases', purchases);
+
+module.exports = router;
