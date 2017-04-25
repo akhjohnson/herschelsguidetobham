1a6a28cc6f3e1a07e13ad1d745978bba9d7eb733
Katie Deck <katielynndeck@gmail.com>
2017-04-25 09:49:41 -0500

    updated locations.proc.js

--- server/procedures/locations.proc.js
+++ server/procedures/locations.proc.js
@@ -1,21 +1,9 @@
 var db = require("../config/db");
 
 exports.all = function() {
-    return db.rows("GetPosts()", []);
-};
-
-exports.write = function(post) {
-    return db.row('InsertPost(?, ?, ?, ?)', [post.title, post.userid, post.categoryid, post.content]);
+    return db.rows("GetAllLocations()", []);
 };
 
 exports.read = function(id) {
-    return db.row('GetSinglePost(?)', [id]);
-};
-
-exports.update = function(post) {
-    return db.empty("UpdatePost(?, ?, ?, ?)", [post.id, post.title, post.categoryid, post.content]);
-};
-
-exports.delete = function(id) {
-    return db.empty("DeletePost(?)", [id]);
+    return db.row('GetSingleLocation(?)', [id]);
 };
