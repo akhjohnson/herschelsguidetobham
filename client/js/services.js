d96272fe3735ab17bf4b88049cf5d992c4d46dd7
Katie Deck <katielynndeck@gmail.com>
2017-04-25 15:18:37 -0500

    updated services

--- client/js/services.js
+++ client/js/services.js
@@ -53,31 +53,31 @@ app.service('UserService', ['$http', '$location', function ($http, $location) {
     }
 }])
 
-app.service("AuthService", ['$http', '$location', function ($http, $location) {
-    var google = require('googleapis');
-    var OAuth2 = google.auth.OAuth2;
+// app.service("AuthService", ['$http', '$location', function ($http, $location) {
+//     var google = require('googleapis');
+//     var OAuth2 = google.auth.OAuth2;
 
-    var oauth2Client = new OAuth2(
-        "346993894176-4mnc5optl7vtv1mdu93c8b7f27kqghl1.apps.googleusercontent.com",
-        "L-5ttDYA8Q7IpDzf-Zr9Z8zx",
-        // NEED REDIRECT
-    );
+//     var oauth2Client = new OAuth2(
+//         "346993894176-4mnc5optl7vtv1mdu93c8b7f27kqghl1.apps.googleusercontent.com",
+//         "L-5ttDYA8Q7IpDzf-Zr9Z8zx",
+//         // NEED REDIRECT
+//     );
 
-    // set auth as a global default
-    google.options({
-        auth: oauth2Client
-    });
-    // generate a url that asks permissions for Google+ and Google Calendar scopes
-    var scopes = [
-        'https://www.googleapis.com/auth/plus.me',
-        'https://www.googleapis.com/auth/calendar'
-    ];
-    var url = oauth2Client.generateAuthUrl({
-        // 'online' (default) or 'offline' (gets refresh_token)
-        access_type: 'offline',
-        // If you only need one scope you can pass it as a string
-        scope: scopes,
-        // Optional property that passes state parameters to redirect URI
-        // state: { foo: 'bar' }
-    });
-}])
+//     // set auth as a global default
+//     google.options({
+//         auth: oauth2Client
+//     });
+//     // generate a url that asks permissions for Google+ and Google Calendar scopes
+//     var scopes = [
+//         'https://www.googleapis.com/auth/plus.me',
+//         'https://www.googleapis.com/auth/calendar'
+//     ];
+//     var url = oauth2Client.generateAuthUrl({
+//         // 'online' (default) or 'offline' (gets refresh_token)
+//         access_type: 'offline',
+//         // If you only need one scope you can pass it as a string
+//         scope: scopes,
+//         // Optional property that passes state parameters to redirect URI
+//         // state: { foo: 'bar' }
+//     });
+// }])
