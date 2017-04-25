9fca0bb0957e5ccf9b88df39fc2947bae9e70b07
James Brett <jmsbrett@gmail.com>
2017-04-25 15:15:34 -0500

    commented some stuff out

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ var cookieParser = require("cookie-parser");
 var api = require('./api')
 var routeMw = require("./middleware/routing.mw");
 var configurePassport = require("./config/passport");
-const express = require('express');
-const simpleOauthModule = require('./../');
+// const express = require('express');
+// const simpleOauthModule = require('./../');
 
 
 
@@ -30,59 +30,59 @@ app.get("*", function(req, res, next) {
     }
 });
 
-const oauth2 = simpleOauthModule.create({
-  client: {
-    id: '<CLIENT_ID>',
-    secret: '<CLIENT_SECRET>',
-  },
-  auth: {
-    tokenHost: 'https://google.com',
-    tokenPath: '/login/oauth/access_token',
-    authorizePath: '/login/oauth/authorize',
-  },
-});
+// const oauth2 = simpleOauthModule.create({
+//   client: {
+//     id: '<CLIENT_ID>',
+//     secret: '<CLIENT_SECRET>',
+//   },
+//   auth: {
+//     tokenHost: 'https://google.com',
+//     tokenPath: '/login/oauth/access_token',
+//     authorizePath: '/login/oauth/authorize',
+//   },
+// });
 
-// Authorization uri definition
-const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'http://localhost:3000/callback',
-  scope: 'notifications',
-  state: '3(#0/!~',
-});
+// // Authorization uri definition
+// const authorizationUri = oauth2.authorizationCode.authorizeURL({
+//   redirect_uri: 'http://localhost:3000/callback',
+//   scope: 'notifications',
+//   state: '3(#0/!~',
+// });
 
-// Initial page redirecting to Github
-app.get('/auth', (req, res) => {
-  console.log(authorizationUri);
-  res.redirect(authorizationUri);
-});
+// // Initial page redirecting to Github
+// app.get('/auth', (req, res) => {
+//   console.log(authorizationUri);
+//   res.redirect(authorizationUri);
+// });
 
-// Callback service parsing the authorization token and asking for the access token
-app.get('/callback', (req, res) => {
-  const code = req.query.code;
-  const options = {
-    code,
-  };
+// // Callback service parsing the authorization token and asking for the access token
+// app.get('/callback', (req, res) => {
+//   const code = req.query.code;
+//   const options = {
+//     code,
+//   };
 
-  oauth2.authorizationCode.getToken(options, (error, result) => {
-    if (error) {
-      console.error('Access Token Error', error.message);
-      return res.json('Authentication failed');
-    }
+//   oauth2.authorizationCode.getToken(options, (error, result) => {
+//     if (error) {
+//       console.error('Access Token Error', error.message);
+//       return res.json('Authentication failed');
+//     }
 
-    console.log('The resulting token: ', result);
-    const token = oauth2.accessToken.create(result);
+//     console.log('The resulting token: ', result);
+//     const token = oauth2.accessToken.create(result);
 
-    return res
-      .status(200)
-      .json(token);
-  });
-});
+//     return res
+//       .status(200)
+//       .json(token);
+//   });
+// });
 
-app.get('/success', (req, res) => {
-  res.send('');
-});
+// app.get('/success', (req, res) => {
+//   res.send('');
+// });
 
-app.get('/', (req, res) => {
-  res.send('Hello<br><a href="/auth">Log in with Github</a>');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello<br><a href="/auth">Log in with Github</a>');
+// });
 
-app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 3000);
