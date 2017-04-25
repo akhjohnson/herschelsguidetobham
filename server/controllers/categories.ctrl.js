f967ee7dd6299b5e5711a1cba82ed7015ce59dbd
Katie Deck <katielynndeck@gmail.com>
2017-04-25 10:22:55 -0500

    more server controllers updates

---
+++ server/controllers/categories.ctrl.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var procedures = require("../procedures/categories.proc");
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+    procedures.all().then(function(success){
+        res.send(success);
+    }, function(err){
+        console.log(err);
+        res.status(500).send(err);
+    })
+});
+
+router.route('/:id')
+    .get(function (req, res) {
+        procedures.read(req.params.id).then(function (success) {
+            res.send(success);
+        }, function (err) {
+            console.log(err);
+            res.status(500).send(err);
+        })
+    });
+
+module.exports = router;
