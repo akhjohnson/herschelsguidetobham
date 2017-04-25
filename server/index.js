7a23d135faa900530ca5dcf8d2823ea99db57a2a
akhjohnson <akhalljohnson@gmail.com>
2017-04-24 20:00:39 -0500

    copied over index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require("cookie-parser");
+var api = require('./api')
+var routeMw = require("./middleware/routing.mw");
+var configurePassport = require("./config/passport");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "../client")));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+configurePassport(app);
+
+app.use('/api', api);
+
+
+app.get("*", function(req, res, next) {
+    if(routeMw.isAsset(req.url)) {
+        next();
+    } else {
+        res.sendFile(path.join(__dirname, "../client/index.html"));
+    }
+});
+
+app.listen(process.env.PORT || 3000);
