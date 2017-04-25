0b06db551d745dab508354c1c58d92a3050bf254
Katie Deck <katielynndeck@gmail.com>
2017-04-25 16:20:54 -0500

    upload controller changes

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -98,8 +98,9 @@ app.controller('uploadController', ['$scope', '$location', 'fileUploadService', 
 
     $scope.uploadFile = function () {
         var file = $scope.myFile;
-        var uploadUrl = "../server/service.php", //Url of webservice/api/server
-            promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
+        var uploadUrl = "../server/service.php"; //Url of webservice/api/server
+        
+        var promise = fileUploadService.uploadFileToUrl(file, uploadUrl);
 
         promise.then(function (response) {
             $scope.serverResponse = response;
