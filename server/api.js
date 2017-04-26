5901ef52080b184a568673342f99034667dc2555
akhjohnson <akhalljohnson@gmail.com>
2017-04-26 11:53:49 -0500

    add eat api

--- server/api.js
+++ server/api.js
@@ -5,6 +5,7 @@ var locations = require('./controllers/locations.ctrl');
 var categories = require('./controllers/categories.ctrl');
 var badges = require('./controllers/badges.ctrl');
 var drink = require('./controllers/drink.ctrl');
+var eat = require('./controllers/eat.ctrl');
 
 
 var router = express.Router();
@@ -15,6 +16,7 @@ router.use('/locations', locations);
 router.use('/categories', categories);
 router.use('/badges', badges);
 router.use('/drink', drink);
+router.use('/eat', eat);
 // router.use('/purchases', purchases);
 
 module.exports = router;
