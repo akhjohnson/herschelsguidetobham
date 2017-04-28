bae97525950f52ce7bcb78a1b5a9820fd4e68629
Katie Deck <katielynndeck@gmail.com>
2017-04-28 14:03:41 -0500

    proc changes

--- server/procedures/play.proc.js
+++ server/procedures/play.proc.js
@@ -8,6 +8,6 @@ exports.read = function(id) {
     return db.row('GetSingleObjective(?)', [id]);
 };
 
-exports.update = function(id) {
-    return db.empty("UpdateObjSuccess(?)", [id]);
+exports.update = function(play) {
+    return db.empty("UpdateObjSuccess(?)", [play.id, play.obj_success]);
 };
